b155fc01f8ed1ef77b087b5881cd07adfd3f02c6
Daniel D. Zhang <idf@users.noreply.github.com>
2016-03-11 23:53:37 -0800

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,6 +1,15 @@
 function compile() {
   if ! javac "${TARGET}/${JAVA_CLASSNAME}.java" 2>> $ret; then
-    echo "${TARGET}/${JAVA_CLASSNAME}.java failed to compile! Exiting..." | tee -a $ret;
+    echo "${TARGET}/${JAVA_CLASSNAME}.java failed to compile! Exiting..." | tee -a $ret
     exit 1
   fi
+}
+
+function exec() {
+    # OSX gtimeout rather than timeout
+    if ! gtimeout 15s java -cp .:*:$TARGET "$JAVA_CLASSNAME" 2>> $ret; then
+        echo "Runtime errors in ${JAVA_CLASSNAME}!" | tee -a $ret
+        exit 1
+    fi
+
 }
