f48af888172810592d438e6cfa0c989f255e5a71
Daniel D. Zhang <idf@users.noreply.github.com>
2016-03-11 23:52:38 -0800

    Update itertools.sh

--- itertools.sh
+++ itertools.sh
@@ -1,6 +1,12 @@
 function iterate_params(){
   # iterate_params list test.sh
   cat $1 | while read line; do
-      bash $2 $line;
+    bash $2 $line
+  done
+}
+
+function iterate_files() {
+  for input in input?.sql; do
+    echo "do something"
   done
 }
