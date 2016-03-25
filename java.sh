d3879586ae82a793d14892fe12cbe14a0297c3e4
Daniel D. Zhang <idf@users.noreply.github.com>
2016-03-25 13:56:53 -0700

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,3 +1,5 @@
+ret="result.txt"
+
 function compile() {
   if ! javac "${TARGET}/${JAVA_CLASSNAME}.java" 2>> $ret; then
     echo "${TARGET}/${JAVA_CLASSNAME}.java failed to compile! Exiting..." | tee -a $ret
