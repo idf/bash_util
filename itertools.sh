a5bf9ab68fd48642c0652afa23aee5c4627249c7
Daniel D. Zhang <idf@users.noreply.github.com>
2016-04-02 12:06:54 -0700

    Update itertools.sh

--- itertools.sh
+++ itertools.sh
@@ -10,3 +10,10 @@ function iterate_files() {
     echo "do something"
   done
 }
+
+function write_file() {
+  # write lines to files 
+  # param 1: number of iterations
+  # param 2: output file path
+  for i in `seq $1`; do echo "$i"; done > $2
+}
