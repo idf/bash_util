9ab43d438d575dc341715ec4e7ee72e6852665ee
Daniel D. Zhang <idf@users.noreply.github.com>
2016-05-27 15:53:36 -0700

    Update io.sh

--- io.sh
+++ io.sh
@@ -5,5 +5,11 @@ function echo_and_file() {
   # &>: stdout & stderr
   
   # echo "message" | tee -a path/to/file;
-  echo $1 | tee -a $2;
+  echo $1 | tee -a $2
+}
+
+function create_rand_file() {
+  # param1: file path
+  # param2: file size in MB
+  dd if=/dev/urandom of=$1 bs=1048576 count=$2
 }
